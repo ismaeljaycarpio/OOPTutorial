8134f62baf59638246799bf7e9fe785224923ec8
ismael-dell <ismaeljaycarpio1992@gmail.com>
2016-07-15 17:56:11 +0800

    initial commit

---
+++ OOPTutorial/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTutorial
+{
+    class Employee
+    {
+        private int _empID;
+
+    }
+}
